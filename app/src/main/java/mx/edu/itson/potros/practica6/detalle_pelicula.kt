de84e825f3ce11784fb7fda5afb157deb234e8f9
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2024-03-07 15:56:25 -0700

    Practica 7

--- app/src/main/java/mx/edu/itson/potros/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/itson/potros/practica6/detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.edu.itson.potros.practica6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -13,12 +15,25 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_image)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val seatLeft: TextView = findViewById(R.id.seatLeft)
+
+
 
         val bundle=intent.extras
         if(bundle!=null){
+
+            var ns = bundle.getInt("numberSeats")
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seatLeft.setText("20 seats available")
+        }
+
+        val button : Button = findViewById(R.id.buyTickets)
+
+        button.setOnClickListener(){
+            var intent: Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
         }
 
 
